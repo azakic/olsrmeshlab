d1ad8954e368c378b6bab31ccebf706bf56f9d04
azakic <azakic@its.rs>
2015-06-25 23:22:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-file = open('test', 'r') #opening file in "test" in read mode
+file = open('nodenumber', 'r') #opening file in "test" in read mode
 x = file.read() 
 z = int(x)
-z = z+1 #Svaki put kada se skripta izvrsi povecava vrednost
-file = open('test', 'w')#otvara fajl "test" u write modu
-x = str(z)#konvertuje promenjivu "z" u string
-file.write(x)#upisuje vrednost u fajl
+z = z+1 
+file = open('nodenumber', 'w')
+x = str(z)
+file.write(x)
