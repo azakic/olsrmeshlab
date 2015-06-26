fd79a450bd26cf6a940f0960b40ae191131cc35f
azakic <azakic@its.rs>
2015-06-26 04:05:33 +0200

    Update createnode.sh

--- createnode.sh
+++ createnode.sh
@@ -6,7 +6,7 @@ file="nodenumber" #the file where you keep your string name
 
 name=$(cat "$file")        #the output of 'cat $file' is assigned to the $name variable
 
-docker run -i --privileged --name node$name -p 888$name:80 -p 222$name:22 -i -t -d azakic/meshlab002 
+docker run -i --privileged --name node$name -p 222$name:22 -i -t -d azakic/olsrmeshlab004 
 #docker start node$name
 docker exec -id node$name squirrel-worker -m 172.17.42.1:1234 -i $name -t tap0
 
